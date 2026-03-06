ac865ecc0107516b95278cd992746f6db20fa319
Michael <mgoronkov23@gmail.com>
2026-03-06 19:52:38 +0500

    init

---
+++ src/main/java/org/prod/bookexchangebackend/BookExchangeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.prod.bookexchangebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExchangeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookExchangeBackendApplication.class, args);
+    }
+
+}
